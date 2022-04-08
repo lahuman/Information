6b008d9ac9d8d482b0d77d95eb019ec688ad87ec
Daniel Lim <lahuman@daum.net>
2022-04-08 16:01:06 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import './App.css';
+import './Information.css';
+
 import Item from './Components/Item';
 
 function App() {
   return (
     <>
-      <section class="u-clearfix u-section-2">
-        <div class="u-clearfix u-expanded-width u-layout-wrap u-layout-wrap-1">
-          <div class="u-layout">
-            <div class="u-layout-row">
-              <Item name="XWIKI" descript="문서 도구" url="#" backgroundColor="#1f42c3" buttonColor="#b3c1f3" />
-              <Item name="REDMINE" descript="이슈트레커" url="#" backgroundColor="#f53978" buttonColor="#fbaec8" />
-            </div>
-          </div>
+      <section class="clearfix section">
+        <div class="layout-row">
+          <Item name="XWIKI" descript="문서 도구" url="#" backgroundColor="#1f42c3" buttonColor="#b3c1f3" />
+          <Item name="REDMINE" descript="이슈트레커" url="#" backgroundColor="#f53978" buttonColor="#fbaec8" />
         </div>
       </section>
-      <section class="u-backlink u-clearfix u-grey-80">
-        <p class="u-text">
+      <footer class="backlink clearfix grey-80">
+        <p class="text">
           <span>ⓒ Digital IT Group. All Rights Reserved.</span>
         </p>
-      </section>
+      </footer>
 
     </>
   );
