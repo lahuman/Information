0402aa0dca8914f90f01492bcf304732b8e3869a
lahuman <lahuman@daum.net>
2022-04-14 12:45:22 +0900

    feat : add guideLink

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './Information.css';
 import Item from './Components/Item';
 
 function App() {
+  const HOST = "https://host";
   return (
     <>
       <section class="clearfix section">
         <div class="layout-row">
-          <Item name="XWIKI" descript="문서 도구" url="#" backgroundColor="#1f42c3" buttonColor="#b3c1f3" />
-          <Item name="REDMINE" descript="이슈트레커" url="#" backgroundColor="#f53978" buttonColor="#fbaec8" />
+          <Item name="XWIKI" descript="문서 도구" url={`${HOST}:8080`} backgroundColor="#1f42c3" buttonColor="#b3c1f3" guideLink={`${HOST}:8080/xwiki/bin/download/Main/WebHome/xwiki_guide.pdf`} />
+          <Item name="REDMINE" descript="이슈트래커" url={`${HOST}:8081`} backgroundColor="#f53978" buttonColor="#fbaec8" guideLink={`${HOST}:8080/xwiki/bin/download/Main/WebHome/redmine_guide.pdf`} />
         </div>
       </section>
       <footer class="backlink clearfix grey-80">
--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,11 +1,12 @@
 
-function Item ({ name, descript, url, backgroundColor, buttonColor }) {
+function Item({ name, descript, url, backgroundColor, buttonColor, guideLink }) {
     // u-size-30 => 50%
     return <div class="align-center container-style size-30 layout-cell" style={{ backgroundColor: `${backgroundColor}`, color: '#ffffff' }} >
         <div class="container-layout u-valign-top container-layout-1" >
             <h2 class="text-1">{name}</h2>
             <p class="text-2">{descript}</p>
             <a href={url} class="btn btn-round hover-palette-3-light-3 radius-50 btn-1" style={{ backgroundColor: buttonColor, color: backgroundColor }}>바로 가기</a>
+            {guideLink && <p><a href={guideLink} style={{ color: '#fff', margin: '10px'}} >사용자 가이드</a></p>}
         </div>
     </div>;
 }
